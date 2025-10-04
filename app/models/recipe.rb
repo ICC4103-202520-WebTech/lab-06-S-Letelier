d9dd82788c067803c89321f15638b301fc3c708e
S-Letelier <siletelier@miuandes.cl.com>
2025-10-04 17:38:03 -0300

    modelo, migraciones, validaciones y crud

---
+++ app/models/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe < ApplicationRecord
+  has_rich_text :instructions
+
+  validates :title, presence: true
+  validates :cook_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :difficulty, presence: true, inclusion: { in: ["Easy", "Medium", "Hard"] }
+end
